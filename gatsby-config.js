8f973e42668b40e10a5112c8525ab4b6f2e31926
Gem Kosan <gem.kosan77@gmail.com>
2021-06-24 15:45:41 -0500

    shortens meta description

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     url: "http://www.webdevgem.com",
     title: "Web Dev Gem",
     author: "Gem Kosan",
-    description: "I'm a frontend developer with a focus on creating usable UIs with React. I develop web pages using modern technologies like JavaScript, React, HTML5, and CSS3. After a foray into UX design, I've returned to web development with a new focus towards usability and accessibility.",
+    description: "I'm a frontend developer with a focus on creating usable UIs with React. I develop web pages using modern technologies like JavaScript, React, HTML5, and CSS3.",
     keywords: ["portfolio", "frontend", "web", "development", "react", "JavaScript", "gatsby"],
     twitter: "",
   },
