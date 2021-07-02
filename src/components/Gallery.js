03d86b54dce7291713c8f386af981254df158321
Gem Kosan <gem.kosan77@gmail.com>
2021-07-02 15:45:02 -0500

    fixes content alignment / margins

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,8 +4,34 @@ import projBooks from "../assets/images/proj-books.png";
 
 const Gallery = (props) => (
 	<section id="gallery">
-		<section id="one" className="tiles">
+		<section id="one" className="tiles inner">
 			<article style={{ backgroundImage: `url(${projBooks})` }}>
+				<header className="major">
+					<h3>Book Search</h3>
+					<p>
+						A small project using Bootstrap 4, React and the Google Books
+						API
+					</p>
+				</header>
+				<Link
+					to="https://gk-books.netlify.app/"
+					className="link primary"
+				><span class="sr-only">Book Search</span></Link>
+			</article>
+            <article style={{ backgroundImage: `url(${projBooks})` }}>
+				<header className="major">
+					<h3>Book Search</h3>
+					<p>
+						A small project using Bootstrap 4, React and the Google Books
+						API
+					</p>
+				</header>
+				<Link
+					to="https://gk-books.netlify.app/"
+					className="link primary"
+				><span class="sr-only">Book Search</span></Link>
+			</article>
+            <article style={{ backgroundImage: `url(${projBooks})` }}>
 				<header className="major">
 					<h3>Book Search</h3>
 					<p>
