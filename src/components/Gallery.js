fe917695aabe33e5cfcbf6daa094e13f01b35014
Gem Kosan <gem.kosan77@gmail.com>
2021-08-18 15:05:58 -0500

    Adds new project: 'Soda POP!'

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { Link } from "gatsby";
+import projSodaPop from "../assets/images/soda-pop-screen.png";
 import projBooks from "../assets/images/proj-books.png";
 
 const Gallery = (props) => (
 	<section id="gallery">
 		<section id="one" className="tiles inner">
+			<article style={{ backgroundImage: `url(${projSodaPop})` }}>
+				<header className="major">
+					<h3>Soda POP!</h3>
+					<p>
+						A simple game were you pop bubbles with song lyrics. Created in vanilla JavaScript, CSS, and HTML. No JS libraries.
+					</p>
+				</header>
+				<Link
+					to="https://soda-pop.netlify.app/"
+					className="link primary"
+				><span class="sr-only">Book Search</span></Link>
+			</article>
 			<article style={{ backgroundImage: `url(${projBooks})` }}>
 				<header className="major">
 					<h3>Book Search</h3>
