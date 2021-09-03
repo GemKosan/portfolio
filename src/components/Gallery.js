746727e459a52166ec377f31335e3b56f527bccc
Gem Kosan <gem.kosan77@gmail.com>
2021-09-02 20:54:30 -0500

    Adds cost savings calculator project

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "gatsby";
 import projSodaPop from "../assets/images/soda-pop-screen.png";
 import projBooks from "../assets/images/proj-books.png";
+import projCSC from "../assets/images/proj-csc.png";
 
 const Gallery = (props) => (
 	<section id="gallery">
@@ -16,7 +17,19 @@ const Gallery = (props) => (
 				<Link
 					to="https://soda-pop.netlify.app/"
 					className="link primary"
-				><span class="sr-only">Book Search</span></Link>
+				><span class="sr-only">Soda Pop!</span></Link>
+			</article>
+			<article style={{ backgroundImage: `url(${projCSC})` }}>
+				<header className="major">
+					<h3>Cost Savings Calculator</h3>
+					<p>
+						A multi-page survey I built for a previous employer. Uses React and processes JSX at run-time.
+					</p>
+				</header>
+				<Link
+					to="https://cost-savings-calculator.netlify.app/"
+					className="link primary"
+				><span class="sr-only">Cost Savings Calculator</span></Link>
 			</article>
 			<article style={{ backgroundImage: `url(${projBooks})` }}>
 				<header className="major">
